96c4c3485d2c7fe3a10c7cb188f9fe6d9fb460c3
Justin Johnson <authalic@gmail.com>
2018-03-30 15:16:30 -0600

    fixed the Serializer to allow POSTing emtpy fields

--- pubinput/serializers.py
+++ pubinput/serializers.py
@@ -1,25 +1,24 @@
 from rest_framework import serializers
 from pubinput.models import PublicComment
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PublicComment
-        fields = (
-            'comment', 
-            'first_name', 
-            'last_name', 
-            'email', 
-            'phone', 
-            'address1', 
-            'address2', 
-            'city', 
-            'state', 
-            'zipcode', 
-            'submitted', 
-            'location', 
-            'bcookie'
-            )
+# This needs to extend Serializer. Don't use ModelSerializer
+# Some of the fields need to be set to allow blank or null as a valid post
 
+class CommentSerializer(serializers.Serializer):
+    comment= serializers.CharField(style={'input_type': 'textarea'})
+    first_name = serializers.CharField(allow_blank=True, allow_null=True)
+    last_name = serializers.CharField(allow_blank=True, allow_null=True) 
+    email = serializers.EmailField(max_length=None, min_length=None, required=False, allow_blank=True)
+    phone = serializers.CharField(allow_blank=True, allow_null=True)
+    address1 = serializers.CharField(allow_blank=True, allow_null=True)
+    address2 = serializers.CharField(allow_blank=True, allow_null=True)
+    city = serializers.CharField(allow_blank=True, allow_null=True)
+    state = serializers.CharField(allow_blank=True, allow_null=True)
+    zipcode = serializers.CharField(allow_blank=True, allow_null=True)
+    location = serializers.CharField(allow_blank=True, allow_null=True)
+
+    def create(self, validated_data):
+        return PublicComment.objects.create(**validated_data)
 
 class ViewSerializer(serializers.ModelSerializer):
     class Meta:
--- pubinput/urls.py
+++ pubinput/urls.py
@@ -3,14 +3,17 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from pubinput import views
 
 
+#  WATCH THE TRAILING SLASHES!!!!!!
+#  THEY NEED TO BE THERE, OR YOU'RE HOSED
+
 app_name = 'pubinput'
 
 urlpatterns = [
    path('comments/', views.CommentList.as_view(), name='commentlist'),
    path('comments/<uuid:pk>', views.CommentView.as_view(), name='commentdetail'),
-   path('addcomment', views.CommentCreateOnly.as_view(), name='commentcreateonly'),  #  WHY DOESN'T THIS WORK?????
-   path('viewcomments', views.CommentListOnly.as_view(), name='commentviewonly'),
-   path('adminview', views.CommentListAdminView.as_view(), name='admincommentview'),
+   path('addcomment/', views.CommentCreateOnly.as_view(), name='commentcreateonly'),  #  WHY DOESN'T THIS WORK?????
+   path('viewcomments/', views.CommentListOnly.as_view(), name='commentviewonly'),
+   path('adminview/', views.CommentListAdminView.as_view(), name='admincommentview'),
 ]
 
 # DRF: Returns a URL pattern list which includes format suffix patterns appended to each of the URL patterns provided.
